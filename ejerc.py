1870f367870e7ad4250c003635627002309bc39d
Raquelolimar <raquelolivenza@gmail.com>
2025-03-26 17:28:20 +0100

    jueves

---
+++ ejerc.py
@@ -0,0 +1,73 @@
+
+import math
+
+1.
+ 
+
+list= ['Alba', 'Ibai', 'Zuri']
+print(list)
+
+tupla= ('claseA', 'grupo de estudiantes', 'redaccion de texto')
+print(tupla)
+
+
+float= 3.14
+print(float)
+
+edad= 18
+print(edad)
+
+from decimal import Decimal
+
+
+valor= 0.30
+print(valor)
+
+
+eat= {
+
+    'breakfast': 'milk',
+    'lunch': 'rice',
+    'dinner': 'chicken'
+
+}
+print(eat)
+
+2
+import math
+
+float = 3.14
+print(round(float))
+
+3
+
+print(math.sqrt(float))
+
+4
+
+breakfast= eat['breakfast']
+print(breakfast)
+
+5
+
+print(tupla[1])
+
+6
+
+list.insert (3, 'Raquel')
+print(list)
+
+7
+
+list[0]= 'Family'
+print(list)
+
+8
+
+list.sort()
+print(list)
+
+9
+
+tupla += ('conclusiones',)
+print(tupla)
